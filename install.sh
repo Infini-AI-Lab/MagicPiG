006894f7314cec8fc540f5662d8002cdfd68a308
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-10-22 22:39:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu121
-pip intall -r requirements.txt
+pip install -r requirements.txt
 cd attention
 pip install -e .
 cd ..
